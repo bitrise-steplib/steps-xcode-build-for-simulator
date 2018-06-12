f13ce38f44cac538c06727b70cd3e40dc17f7ec3
Akos Birmacher <akos.birmacher@bitrise.io>
2018-06-12 10:13:30 +0200

    Step inputs added

---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// xcodebuild build -workspace ~/Develop/XCode/XcodeArchiveTest/XcodeArchiveTest.xcworkspace -scheme Other -configuration Debug -sdk iphonesimulator11.4 -derivedDataPath ./ddata
+
+}
