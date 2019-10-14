97b4d1727f0614dc201155bc37a0679ac612c1a3
David McRae <dmcrae@d3banking.com>
2019-10-14 13:15:25 -0500

    switching how to join strings

--- main.go
+++ main.go
@@ -548,7 +548,7 @@ func exportArtifacts(proj xcodeproj.XcodeProj, scheme string, schemeBuildDir str
 				} else if !exists {
 					log.Debugf("path not exists: %s", source)
 					// Also check to see if a path exists with the target name
-					source := filepath.Join(sourceDir, strings.Join(target.Name, ".app"))
+					source := filepath.Join(sourceDir, target.Name + ".app")
 
 					if exists, err := pathutil.IsPathExists(source); err != nil {
 						log.Debugf("failed to check if the path exists: (%s), error: ", source, err)
