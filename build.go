d4cfaea41b0f9d21255e89ef983fe21c67aca6bd
lpusok <7979773+lpusok@users.noreply.github.com>
2019-10-30 09:33:45 +0100

    Tool 1126 swiftpm (#18)
    * Adding caching for Swift PM packages
    
    * dep update
    
    * Updating code to match xcode-project interface
    
    * Fixing xcpretty version check
    
    * Removing xcode8 sample project test
    
    * Disable react-native tests
    
    * Seperating prepareCommand from runCommand
    
    * Printing correct xcpretty command before build in log.
    
    * Fixing step name in logging
    
    * Removing typo
    
    * Adding swift pm cache input
    
    * Do not run swiftpm project
    
    * Restore logging
    
    * Only cache artifacts if export is successful.

---
+++ build.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/colorstring"
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-xcode/xcodebuild"
+	cache "github.com/bitrise-io/go-xcode/xcodecache"
+	"github.com/bitrise-io/go-xcode/xcpretty"
+	"github.com/bitrise-steplib/steps-xcode-build-for-simulator/util"
+)
+
+func runCommandWithRetry(cmd *xcodebuild.CommandBuilder, useXcpretty bool, swiftPackagesPath string) (string, error) {
+	output, err := runCommand(cmd, useXcpretty)
+	if err != nil && swiftPackagesPath != "" && strings.Contains(output, cache.SwiftPackagesStateInvalid) {
+		log.Warnf("Build failed, swift packages cache is in an invalid state, error: %s", err)
+		log.RWarnf("xcode-build-for-simulator", "swift-packages-cache-invalid", nil, "swift packages cache is in an invalid state")
+		if err := os.RemoveAll(swiftPackagesPath); err != nil {
+			return output, fmt.Errorf("failed to remove invalid Swift package caches, error: %s", err)
+		}
+		return runCommand(cmd, useXcpretty)
+	}
+	return output, err
+}
+
+func prepareCommand(xcodeCmd *xcodebuild.CommandBuilder, useXcpretty bool, output *bytes.Buffer) (*command.Model, *xcpretty.CommandModel) {
+	if useXcpretty {
+		return nil, xcpretty.New(*xcodeCmd)
+	}
+
+	buildRootCmd := xcodeCmd.Command()
+	buildRootCmd.SetStdout(io.MultiWriter(os.Stdout, output))
+	buildRootCmd.SetStderr(io.MultiWriter(os.Stderr, output))
+	return buildRootCmd, nil
+}
+
+func runCommand(buildCmd *xcodebuild.CommandBuilder, useXcpretty bool) (string, error) {
+	var output bytes.Buffer
+	xcodebuildCmd, xcprettyCmd := prepareCommand(buildCmd, useXcpretty, &output)
+
+	if xcprettyCmd != nil {
+		util.LogWithTimestamp(colorstring.Green, "$ %s", xcprettyCmd.PrintableCmd())
+		fmt.Println()
+		return xcprettyCmd.Run()
+	}
+	util.LogWithTimestamp(colorstring.Green, "$ %s", xcodebuildCmd.PrintableCommandArgs())
+	fmt.Println()
+	return output.String(), xcodebuildCmd.Run()
+}
