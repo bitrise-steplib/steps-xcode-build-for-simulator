cf31501fccaea998aeab6a9385e1f5515e88376b
Balázs Hajagos <balazs.hajagos@bitrise.io>
2025-09-16 14:26:40 +0200

    Fix artifact name

--- main.go
+++ main.go
@@ -233,7 +233,7 @@ func createRunOptions(config Config) archive.RunOpts {
 		DestinationPlatform: platform,
 		Configuration:       config.Configuration,
 		XcodeMajorVersion:   config.XcodeMajorVersion,
-		ArtifactName:        config.Scheme + "-simulator.xcarchive",
+		ArtifactName:        config.Scheme + "-simulator",
 
 		CodesignManager: nil,
 
